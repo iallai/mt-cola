87a9a9036b3bef1bb51b4febd2729480148cb895
iallai <iallai.info@gmail.com>
2018-07-12 17:53:13 +0800

    feat: Add Subtitle

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,13 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('猫图可乐 - 喜欢哪一杯'),
+        title: new Text('猫图可乐 - 智慧工单系统'),
       ),
       body: _buildSuggestions(),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: '添加',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -45,21 +49,24 @@ class RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
-        return _buildRow(_suggestions[index]);
+        return _buildRow(_suggestions[index],index);
       },
     );
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow(WordPair pair,int index) {
     final beSaved = _saved.contains(pair);
+    final _index =index.toString();
+    // final index =index;
     return new ListTile(
       title: new Text(
-        pair.asPascalCase,
+        "工单"+_index,
         style: _biggerFont,
       ),
+      subtitle: new Text("描述"+pair.asPascalCase),
       // c: new Text("1"),
       trailing:new Icon(
-        beSaved?Icons.favorite:Icons.favorite_border,
+        beSaved?Icons.alarm_on:Icons.alarm,
         color: beSaved?Colors.red:null
       ),
       onTap: (){
