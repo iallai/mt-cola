b4d64a99f03e4f90c2310aeb973f0754778f7281
iallai <iallai.info@gmail.com>
2018-07-15 11:19:33 +0800

    feat(main): http
    Closes #1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() => runApp(new MyApp());
 
@@ -24,6 +26,7 @@ class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _saved = new Set<WordPair>();
   final TextStyle _biggerFont = new TextStyle(fontSize: 18.0);
+  var httpClient = new HttpClient();
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +76,7 @@ class RandomWordsState extends State<RandomWords> {
         setState(() {
                   if(beSaved){
                     _saved.remove(pair);
+                    _getIPAddress();
                   }else{
                     _saved.add(pair);
                   }
@@ -81,3 +85,31 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 }
+
+_getIPAddress() async {
+    var url = 'http://www.mocky.io/v2/5b49e01631000072138bc08a';
+    var httpClient = new HttpClient();
+
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      print(response);
+      if (response.statusCode == HttpStatus.OK) {
+        var json = await response.transform(UTF8.decoder).join();
+        var data = JSON.decode(json);
+        result = data['orders'];
+      } else {
+        result =
+            'Error getting IP address:\nHttp status ${response.statusCode}';
+      }
+    } catch (exception) {
+      print(exception);
+      result = 'Failed getting IP address';
+    }
+    print(result);
+    // If the widget was removed from the tree while the message was in flight,
+    // we want to discard the reply rather than calling setState to update our
+    // non-existent appearance.
+  
+  }
